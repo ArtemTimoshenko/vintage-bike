6e492cd847a58d400a35bc5351f199cf8410d0da
ArtemTimoshenko <tumosh2f@gmail.com>
2015-10-13 02:18:36 +0300

    JavaScript Responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 (function(){
-	var menuToggle = document.getElementsByClassTagName("ba-menu-toggle");
+	var menuToggle = document.getElementsByClassName("ba-menu-toggle");
+
 	var body = document.getElementsByTagName("body")[0];
 
-	menuToggle[0].onclick = function(){
-		body.classlist.toggle("ba-menu-open");
+	console.log(body);
+	console.log(menuToggle);
+
+	menuToggle[0].onclick = function (){
+		body.classList.toggle("ba-menu-open");
 	};
 })();
